717aecdf9c365d2b47de54d37d57cda1b446ccb2
Victor Rodriguez <vicrerdgz@gmail.com>
2021-04-28 22:44:13 +0200

    Adding schemas source file

---
+++ dendroam-schemas.el
@@ -0,0 +1,27 @@
+;;; dendroam-schemas.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Victor Rodriguez
+;;
+;; Author: Victor Rodriguez <https://github.com/vrodriguez>
+;; Maintainer: Victor Rodriguez <vrodriguez@confluent.io>
+;; Created: April 28, 2021
+;; Modified: April 28, 2021
+;; Version: 0.0.1
+;; Keywords: Symbol’s value as variable is void: finder-known-keywords
+;; Homepage: https://github.com/vrodriguez/dendroam-schemas
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'dendroam-schemas)
+
+
+;;; dendroam-schemas.el ends here
